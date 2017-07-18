b71043d3d20f6b8cd0091f2b6772c8a591b04492
Krzysztof Zielinski <krzysztof.zielinski.work@gmail.com>
2017-07-18 22:50:24 +0200

    added bootstrap navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import AppNav from './components/app-nav-view';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <AppNav />
         <h1>Hello World </h1>
       </div>
     );
