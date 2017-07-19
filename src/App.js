51dc353abcd01c45ca7da735cc85659ab7310d3c
Krzysztof Zielinski <krzysztof.zielinski.work@gmail.com>
2017-07-19 23:28:48 +0200

    user-view styles improvement

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import AppNav from './components/app-nav-view';
 
+import './css/app.css';
+
 class App extends Component {
   render() {
     return (
--- src/components/users-view.js
+++ src/components/users-view.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import users from './../data/users.json';
+import { Grid, Row, Col, Table } from 'react-bootstrap';
 
 export default class UsersView extends React.Component {
   constructor(props) {
@@ -12,18 +13,43 @@ export default class UsersView extends React.Component {
 
   render() {
     return (
-      <div>
+      <Grid>
+        <Row>
+          <Col>
         <h1>UsersView</h1>
         <p> {this.state.test} </p>
 
-        <div>
-          {
-            users && users.map((user, index) =>
-            <p key={index}> <img src={user.avatar} alt={user.first_name} /> {user.id}. {user.first_name} {user.last_name} {user.gender} {user.age} {user.grade}  </p>
-            )
-          }
-        </div>
-      </div>
+        <Table striped bordered condensed >
+          <thead>
+            <tr>
+              <th>l.p.</th>
+              <th>Avatar</th>
+              <th>Firts name</th>
+              <th>Last name</th>
+              <th>Gender</th>
+              <th>Age</th>
+              <th>Grade</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              users && users.map((user, index) =>
+                <tr key={index}>
+                  <td key={index + 1}>{user.id}.</td>
+                  <td key={index}><img src={user.avatar} alt={user.first_name} /></td>
+                  <td key={index + 2}>{user.first_name}</td>
+                  <td key={index + 3}>{user.last_name}</td>
+                  <td key={index + 4}>{user.gender}</td>
+                  <td key={index + 5}>{user.age}</td>
+                  <td key={index + 6}>{user.grade}</td>
+                </tr>
+              )
+            }
+          </tbody>
+        </Table>
+        </Col>
+        </Row>
+      </Grid>
     )
   }
 }
